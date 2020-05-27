e369a97f2565608a15d49a6dd8fb1f84f249b9df
Yoni Harris <58910385+yoniharris@users.noreply.github.com>
2020-05-27 13:11:28 +0300

    Create SQLSample.sql

---
+++ SQLSample.sql
@@ -0,0 +1,7 @@
+ SELECT "tbl_Tags".client_id,
+    "tbl_Tags".subzone_id,
+    ( SELECT max("tbl_Tags".last_seen) AS max) AS last_updated
+   FROM "tbl_Tags"
+  WHERE "tbl_Tags".client_id = ANY (ARRAY[current_setting('application_name'::text)::integer, 0])
+  GROUP BY "tbl_Tags".client_id, "tbl_Tags".subzone_id
+  ORDER BY (( SELECT max("tbl_Tags".last_seen) AS max)) DESC;
